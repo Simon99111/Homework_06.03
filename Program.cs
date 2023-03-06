bb93bd83736975a28e8cb0e8c2f948dfa6eba6d6
Семён Курочкин <sema.kurochkin@mail.ru>
2023-03-06 22:39:51 +0300

    Файл с решенными задачами

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Shares;
+
+//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// int row = Input ("Введите количество строк");
+// int column = Input ("Введите количество столбцов");
+// int [,] table = FillArray(row, column);
+// Print(table);
+// Sort (table);
+// Console.WriteLine();
+// Print (table);
+
+
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// int row = Input ("Введите количество строк");
+// int column = Input ("Введите количество столбцов");
+// int [,] table = FillArray(row, column);
+// Print(table);
+// Console.WriteLine();
+// SummMinimum(table);
+
+
+
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// Console.WriteLine("Введите первый двумерный массив");
+// int row1 = Input("Введите количество строк");
+// int column1 = Input("Введите количество столбцов");
+// int[,] matrix1 = ArrayKeyBoard(row1, column1);
+// Console.WriteLine("Введите второй двумерный массив");
+// int row2 = Input("Введите количество строк");
+// int column2 = Input("Введите количество столбцов");
+// int[,] matrix2 = ArrayKeyBoard(row2, column2);
+// int[,] a = Multi(matrix1, matrix2);
+// Print(a);
+
+
+
+/* 
+* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+* которая будет построчно выводить массив, добавляя индексы каждого элемента.
+* Массив размером 2 x 2 x 2
+*/
+
+// int [,,] matrixCube = CubeArray();
+// PrintCube(matrixCube);
+
+
+
+
+
